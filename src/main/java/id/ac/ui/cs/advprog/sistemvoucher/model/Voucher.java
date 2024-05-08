c08356e1ea792ec6cf6f4bb1d4cbbc88b3d257dc
marvelm57 <vivalamarvel@gmail.com>
2024-05-08 17:04:45 +0700

    [GREEN] Implement Voucher model

--- src/main/java/id/ac/ui/cs/advprog/sistemvoucher/model/Voucher.java
+++ src/main/java/id/ac/ui/cs/advprog/sistemvoucher/model/Voucher.java
@@ -34,14 +34,30 @@ public class Voucher {
     }
 
     public Voucher(long voucherId, String voucherName, int discountAmount) {
+        this.voucherId = voucherId;
+        this.voucherName = voucherName;
+        setDiscountAmount(discountAmount);
+        this.maxUsage = Integer.MAX_VALUE;
     }
 
     public Voucher(long voucherId, String voucherName, int discountAmount, long maxUsage) {
+        this.voucherId = voucherId;
+        this.voucherName = voucherName;
+        setDiscountAmount(discountAmount);
+        this.maxUsage = maxUsage;
     }
 
     public void setDiscountAmount(int discountAmount) {
+        if (discountAmount < 0 || discountAmount > 100) {
+            throw new IllegalArgumentException();
+        } else {
+            this.discountAmount = discountAmount;
+        }
     }
 
     public void updateNoOfUsed() {
+        if (this.maxUsage > this.noOfUsed) {
+            this.noOfUsed++;
+        }
     }
 }
