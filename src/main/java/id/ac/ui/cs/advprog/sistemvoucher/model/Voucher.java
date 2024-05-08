34390c0a7dcb1dcfa3709d0c346928d9f25c7a23
marvelm57 <vivalamarvel@gmail.com>
2024-05-08 17:01:01 +0700

    [REFACTOR] Change Voucher model skeleton

--- src/main/java/id/ac/ui/cs/advprog/sistemvoucher/model/Voucher.java
+++ src/main/java/id/ac/ui/cs/advprog/sistemvoucher/model/Voucher.java
@@ -3,13 +3,45 @@ package id.ac.ui.cs.advprog.sistemvoucher.model;
 import lombok.Getter;
 import lombok.Setter;
 
-@Getter @Setter
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
+@Entity
+@Table(name = "vouchers")
+@Getter
 public class Voucher {
-    private String voucherId;
+    @Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "voucher_id")
+    @NotNull
+    private long voucherId;
+
+    @Column(name = "voucher_name")
+    @NotNull
     private String voucherName;
-    private String discountType;
-    private double discountAmount;
-    private String usageType;
-    private int maxUsage;
-    private int totalUsed;
+
+    @NotNull
+    @Column(name = "discount_amount")
+    private int discountAmount;
+
+    @Column(name = "max_usage")
+    private long maxUsage;
+
+    @Column(name = "noOfUsed")
+    private long noOfUsed;
+
+    public Voucher() {
+    }
+
+    public Voucher(long voucherId, String voucherName, int discountAmount) {
+    }
+
+    public Voucher(long voucherId, String voucherName, int discountAmount, long maxUsage) {
+    }
+
+    public void setDiscountAmount(int discountAmount) {
+    }
+
+    public void updateNoOfUsed() {
+    }
 }
